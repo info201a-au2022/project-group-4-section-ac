e190a4a499c6833dbcac3417cf21cbe4adc3b9f0
sjahed7 <sjahed7@uw.edu>
2022-12-04 11:57:50 -0800

    major apps

--- app_server.R
+++ app_server.R
@@ -17,6 +17,24 @@ olympic_volleyball_medals <- olympic_medals %>%
 
 # Sabrina
 
+# value --- which country has earned the most medals
+most_medals <- medals_over_years %>% 
+  group_by(country_3_letter_code) %>% 
+  summarize(cumul_medals = sum(total_medals)) %>% 
+  arrange(desc(cumul_medals)) %>% 
+  head(1) %>% 
+  pull(country_3_letter_code)
+
+# ctry_choices <- medals_over_years %>% pull(country_3_letter_code) %>%  unique()
+  
+ctry_choices <- medals_over_years %>% 
+  group_by(country_3_letter_code) %>% 
+  summarize(cumul_medals = sum(total_medals)) %>% 
+  arrange(desc(cumul_medals)) %>% 
+  head(5) %>% 
+  pull(country_3_letter_code)
+
+
 # Start shinyServer
 server <- function(input, output) { 
   # Harman
@@ -68,4 +86,20 @@ server <- function(input, output) {
   })
   
   # Sabrina
+    output$mostMedals <- renderText(most_medals)
+    output$countryCheck <- renderPlotly({
+      country_df <- medals_over_years %>% 
+        filter(country_3_letter_code %in% input$countryCheck)
+      f <- ggplot(
+        data = country_df, aes(
+          x = year_oly, y = total_medals,
+          group = 1
+        ),
+        na.rm = TRUE
+      ) +
+        geom_line(aes(color = country_3_letter_code))
+      labs(title = "Medals Earned each Olympic Games") +
+        xlab("Year") + ylab("Medals Earned")
+      return(f)
+    })
 }
